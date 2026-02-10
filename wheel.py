ac0eab901188fd755b007f1babf8476505e5b218
kygiese <kygiese7@gmail.com>
2026-02-09 23:03:27 -0700

    copied from main

--- wheel.py
+++ wheel.py
@@ -13,17 +13,31 @@ class Wheel:
         self.motor.setRange(LEFT_WHEEL, min, max)
         self.motor.setRange(RIGHT_WHEEL, min, max)
 
-    def forward(self, n):
-        self.motor.setTarget(LEFT_WHEEL, 4000)
-        self.motor.setTarget(RIGHT_WHEEL, 4000)
-        sleep(n)
-        self.motor.setTarget(LEFT_WHEEL, CENTER)
-        self.motor.setTarget(RIGHT_WHEEL, CENTER)
+    def forward(self, time, speed):
+        self.motor.setTarget(LEFT_WHEEL, CENTER-speed)
+        self.motor.setTarget(RIGHT_WHEEL, CENTER+speed)
+        sleep(time)
+        self.stop()
 
 
-    def backward(self, n):
+    def backward(self, time, speed):
+        self.motor.setTarget(LEFT_WHEEL, CENTER+speed)
+        self.motor.setTarget(RIGHT_WHEEL, CENTER+speed)
+        sleep(time)
+        self.stop()
+
+    def turn_left(self, time, speed):
+        self.motor.setTarget(LEFT_WHEEL, 5000)
+        self.motor.setTarget(RIGHT_WHEEL, 5000)
+        sleep(time)
+        self.stop()
+
+    def turn_right(self, time, speed):
         self.motor.setTarget(LEFT_WHEEL, 7000)
         self.motor.setTarget(RIGHT_WHEEL, 7000)
-        sleep(n)
+        sleep(time)
+        self.stop()
+
+    def stop(self):
         self.motor.setTarget(LEFT_WHEEL, CENTER)
         self.motor.setTarget(RIGHT_WHEEL, CENTER)
