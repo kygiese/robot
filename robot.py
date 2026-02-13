7fc7226a3845f7dd2fda8d210488b8064fed16c2
kygiese <kygiese7@gmail.com>
2026-02-12 23:22:05 -0700

    AAAAAAAAAAAAAAAAA

--- robot.py
+++ robot.py
@@ -44,4 +44,38 @@ class Robot:
         sleep(2)
         self.waist.center()
         sleep(1)
+    
+    def   def move_waist(self, target):
+        self.waist.turn(target)
 
+    def tilt_head(self, target):
+        self.head.tilt(target)
+
+    def pan_head(self, target):
+        self.head.pan(target)
+
+    def center_all(self):
+        self.waist.center()
+        self.head.center()
+
+    def forward(self, time, speed):
+        self.wheels.stop()
+        self.wheels.forward(time, speed)
+
+    def backward(self, time, speed):
+        self.wheels.stop()
+        self.wheels.backward(time, speed)
+
+    def turn_left(self, time, speed):
+        self.wheels.stop()
+        self.wheels.turn_left(time, speed)
+
+    def turn_right(self, time, speed):
+        self.wheels.stop()
+        self.wheels.turn_right(time, speed)
+
+    def move_by_vector(self, left, right, time):
+        self.wheels.stop()
+        self.wheels.move(left, right)
+        sleep(time)
+        self.wheels.stop()
