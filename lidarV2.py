21ec0b6ab130708d637bf2223d44e23ab5e1144c
kygiese <kygiese7@gmail.com>
2026-04-06 12:41:02 -0600

    object

--- lidarV2.py
+++ lidarV2.py
@@ -1,75 +1,82 @@
 from pyrplidar import PyRPlidar
+from math import floor
 import time
 
-def get_info():
-    lidar = PyRPlidar()
-    lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
-    # Linux   : "/dev/ttyUSB0"
-    # MacOS   : "/dev/cu.SLAB_USBtoUART"
-    # Windows : "COM5"
 
-    info = lidar.get_info()
-    print("info :", info)
+class Lidar:
+    def __init__(self):
+        self.checkB = True
+        self.checkF = True
 
-    health = lidar.get_health()
-    print("health :", health)
+    def get_info(self):
+        lidar = PyRPlidar()
+        lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
+        # Linux   : "/dev/ttyUSB0"
+        # MacOS   : "/dev/cu.SLAB_USBtoUART"
+        # Windows : "COM5"
 
-    samplerate = lidar.get_samplerate()
-    print("samplerate :", samplerate)
+        info = lidar.get_info()
+        print("info :", info)
 
-    scan_modes = lidar.get_scan_modes()
-    print("scan modes :")
-    for scan_mode in scan_modes:
-        print(scan_mode)
+        health = lidar.get_health()
+        print("health :", health)
 
-    lidar.disconnect()
+        samplerate = lidar.get_samplerate()
+        print("samplerate :", samplerate)
 
-def simple_scan():
-    lidar = PyRPlidar()
-    lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
+        scan_modes = lidar.get_scan_modes()
+        print("scan modes :")
+        for scan_mode in scan_modes:
+            print(scan_mode)
 
-    lidar.set_motor_pwm(500)
-    time.sleep(2)
+        lidar.disconnect()
 
-    scan_generator = lidar.force_scan()
+    def simple_scan(self):
+        lidar = PyRPlidar()
+        lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
 
-    for count, scan in enumerate(scan_generator()):
-        print(count, scan)
-        if count == 20: break
+        lidar.set_motor_pwm(500)
+        time.sleep(2)
 
-    lidar.stop()
-    lidar.set_motor_pwm(0)
+        scan_generator = lidar.force_scan()
 
-    lidar.disconnect()
+        for count, scan in enumerate(scan_generator()):
+            print(count, scan)
+            if count == 20: break
 
-def lidar_scan():
-    lidar = PyRPlidar()
-    lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
+        lidar.stop()
+        lidar.set_motor_pwm(0)
 
-    lidar.set_motor_pwm(500)
-    time.sleep(2)
+        lidar.disconnect()
 
-    scan_generator = lidar.force_scan()
-    scan_data = [0] * 360
+    def lidar_scan(self):
+        lidar = PyRPlidar()
+        lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
 
-    for count, scan in enumerate(scan_generator()):
-        for (_, angle, distance) in scan:
-            scan_data[min([359, floor(angle)])] = distance
-            if 240 < angle < 300:
-                if distance < 600:
+        lidar.set_motor_pwm(500)
+        time.sleep(2)
+
+        scan_generator = lidar.force_scan()
+        scan_data = [0] * 360
+
+        for count, scan in enumerate(scan_generator()):
+            scan_data[min([359, floor(scan[2])])] = scan[3]
+            if 240 < scan[2] < 300:
+                if scan[3] < 600:
                     self.checkF = True
-            elif 100 < angle < 160:
-                if distance < 600:
+            elif 100 < scan[2] < 160:
+                if scan[3] < 600:
                     self.checkB = True
-        print(self.checkF, self.checkB)
-        self.checkF = False
-        self.checkB = False
 
+            print(self.checkF, self.checkB)
+            self.checkF = False
+            self.checkB = False
 
-    lidar.stop()
-    lidar.set_motor_pwm(0)
+        lidar.stop()
+        lidar.set_motor_pwm(0)
 
-    lidar.disconnect()
+        lidar.disconnect()
 
 if __name__ == "__main__":
-    simple_scan()
+    lidar = Lidar()
+    lidar.simple_scan()
