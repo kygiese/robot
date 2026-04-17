1147ce911d1ecfcef39544149d8f3f5dbfb64d09
kygiese <kygiese7@gmail.com>
2026-04-16 23:47:07 -0600

    ssssssssssssss

--- lidarV2.py
+++ lidarV2.py
@@ -48,7 +48,7 @@ class Lidar:
 
         for count, scan in enumerate(scan_generator()):
             print(count, scan)
-            if count == 20: break
+            if count == 50: break
 
         lidar.stop()
         lidar.set_motor_pwm(0)
