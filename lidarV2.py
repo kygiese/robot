34af9fc0a116fc34c68c1e4e8789aa4d85fb7914
kygiese <kygiese7@gmail.com>
2026-04-16 21:30:31 -0600

    ssssssssssssss

--- lidarV2.py
+++ lidarV2.py
@@ -13,16 +13,11 @@ class Lidar:
 
 
     def get_info(self):
-        self.lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
+        self.lidar.connect(port="/dev/ttyUSB0", baudrate=256000, timeout=3)
         # Linux   : "/dev/ttyUSB0"
         # MacOS   : "/dev/cu.SLAB_USBtoUART"
         # Windows : "COM5"
-        self.lidar.set_motor_pwm(500)
-        time.sleep(2)
 
-        self.lidar.set_motor_pwm(0)
-
-        self.lidar.disconnect()
         info = self.lidar.get_info()
         print("info :", info)
 
@@ -125,4 +120,3 @@ class Lidar:
 if __name__ == "__main__":
     lidar = Lidar()
     lidar.simple_scan()
-
