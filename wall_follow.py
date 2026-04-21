f6caf4ab5c8375eb4ea7ad96088feb234a226cd5
kygiese <kygiese7@gmail.com>
2026-04-20 22:18:42 -0600

    new wall follow method

--- wall_follow.py
+++ wall_follow.py
@@ -28,7 +28,7 @@ def find_speeds(scan_data, default_speed):
 
     r = (2*math.sin(alpha))/ld
 
-    print(r)
+    print(alpha)
 
     right_speed = ((default_speed*r*2) - (w*default_speed))/((r*2)+w)
 
