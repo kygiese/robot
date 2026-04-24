e67670a70cd5a7d79784ee30adb32208600154b1
BigChalupa <solve.d.riddles@gmail.com>
2026-04-23 20:56:23 -0600

    Update lidarV2.py

--- lidarV2.py
+++ lidarV2.py
@@ -119,8 +119,8 @@ class Lidar:
 
                 if count % 360 == 0 and count > 1 and self.robot.FollowOn:
 
-                    left_speed, right_speed = wall_follow.find_speeds(scan_data, -50)
-                    self.robot.drive(left_speed, right_speed, self.robot.FollowMode)
+                    left_speed, right_speed = wall_follow.find_speeds(scan_data, -50, self.robot.FollowMode)
+                    self.robot.drive(left_speed, right_speed)
 
 
                     '''
