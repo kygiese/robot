49a8ba04b69a44805a57d7f2c8d03859c260f15f
kygiese <kygiese7@gmail.com>
2026-04-28 00:39:06 -0600

    added listening function (written by claude and really bad)

--- lidarV2.py
+++ lidarV2.py
@@ -123,12 +123,12 @@ class Lidar:
 
                     if self.robot.FollowMode:
                     #print(left_speed, right_speed)
-                        if  scan_data[180] > 1200:
+                        if  scan_data[180] > 1400:
                                 self.intersect_flag = True
                         else:
                             self.intersect_flag = False
                     else:
-                        if  scan_data[0] > 1200:
+                        if  scan_data[0] > 1400:
                                 self.intersect_flag = True
                         else:
                             self.intersect_flag = False
